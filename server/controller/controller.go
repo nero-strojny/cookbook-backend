eee7edffe60aaa1af2afea18902daf544ba1c831
jstrojny <jakestrojny@gmail.com>
2020-06-22 19:08:00 -0400

    React skeleton (#18)
    * Create React skeleton
    
    * Issue-11: Restructure skeleton and better define recipe api

---
+++ server/controller/controller.go
@@ -0,0 +1,26 @@
+package controller
+
+import(
+	"../models"
+)
+
+//Get a recipe by its ID
+func Get(id int) {
+
+}
+
+//Delete a recipe by its ID. This does not permanently delete and instead marks the recipe to be hidden
+//so that it can be restored if necessary
+func Delete(id int) {
+
+}
+
+//Create a new recipe
+func Create(recipe models.Recipe) {
+
+}
+
+//Update an existing recipe by its id
+func Update(id int, updatedRecipe models.Recipe) {
+
+}
--- server/models/models.go
+++ server/models/models.go
@@ -1,8 +1,28 @@
 package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
-type ToDoList struct {
- 
-  ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-  Task   string             `json:"task,omitempty"`
-  Status bool               `json:"status,omitempty"`
+
+// Recipe is used to marshal and unmarshal from a document database
+type Recipe struct {
+  RecipeID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+  CreatedDate   string             `json:"createdDate,omitempty"`
+  LastUpdatedDate string               `json:"lastUpdatedDate,omitempty"`
+  Ingredients []ingredient `json:"ingredients,omitempty"`
+  Author string `json:"author,omitempty"`
+  PrepTime int `json:"prepTime,omitempty"`
+  CookTime int `json:"cookTime,omitempty"`
+  Steps []step `json:"steps,omitempty"`
+
+}
+
+// Ingredient is a component of a recipe consisting of the name, amount, and the measurement for that amount (cups, tbsp, lbs, etc)
+type ingredient struct {
+  Name string `json:"name,omitempty"`
+  Amount float32 `json:"amount,omitempty"`
+  Measurement string `json:"measurement,omitempty"`
+}
+
+// Step is what to do in order for a recipe
+type step struct {
+  Number int `json:"number,omitempty"`
+  Text string `json:"text,omitEmpty"`
 }
