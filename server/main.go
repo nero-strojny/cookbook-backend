db612dcfee0ff3fdf3fc2c9f594e792c5b10133f
Jake Strojny <jakestrojny@gmail.com>
2020-06-26 15:40:41 -0400

    Fix routing to handle CORS preflight OPTIONS requests

--- server/main.go
+++ server/main.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
+
 	"server/router"
 )
 
 func main() {
 	r := router.Router()
 	fmt.Println("Starting server on the port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS()(r)))
 }
