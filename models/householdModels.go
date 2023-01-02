ff7ef572ccfce436f0c080adca85be9bccebf170
Jake Strojny <jakestrojny@gmail.com>
2023-01-02 15:22:07 -0600

    Create calendars with random recipes instead of based on a passed in calendar

--- models/householdModels.go
+++ models/householdModels.go
@@ -16,11 +16,11 @@ type Calendar struct {
 	CalendarID  primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	HouseholdID primitive.ObjectID `json:"householdID,omitempty" bson:"householdID,omitempty"`
 	StartDate   string             `json:"startDate,omitempty"`
-	Monday      primitive.ObjectID `json:"monday,omitempty" bson:"monday,omitempty"`
-	Tuesday     primitive.ObjectID `json:"tuesday,omitempty" bson:"tuesday,omitempty"`
-	Wednesday   primitive.ObjectID `json:"wednesday,omitempty" bson:"wednesday,omitempty"`
-	Thursday    primitive.ObjectID `json:"thursday,omitempty" bson:"thursday,omitempty"`
-	Friday      primitive.ObjectID `json:"friday,omitempty" bson:"friday,omitempty"`
-	Saturday    primitive.ObjectID `json:"saturday,omitempty" bson:"saturday,omitempty"`
-	Sunday      primitive.ObjectID `json:"sunday,omitempty" bson:"sunday,omitempty"`
+	Monday      Recipe             `json:"monday,omitempty" bson:"monday,omitempty"`
+	Tuesday     Recipe             `json:"tuesday,omitempty" bson:"tuesday,omitempty"`
+	Wednesday   Recipe             `json:"wednesday,omitempty" bson:"wednesday,omitempty"`
+	Thursday    Recipe             `json:"thursday,omitempty" bson:"thursday,omitempty"`
+	Friday      Recipe             `json:"friday,omitempty" bson:"friday,omitempty"`
+	Saturday    Recipe             `json:"saturday,omitempty" bson:"saturday,omitempty"`
+	Sunday      Recipe             `json:"sunday,omitempty" bson:"sunday,omitempty"`
 }
